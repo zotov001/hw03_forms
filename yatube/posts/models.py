e0ffee54fb84bf69dd9e8a0c372a41bd3c3aa58a
Марк Зотов <zotov001@yandex.ru>
2022-06-22 00:04:03 +0300

    hw03 tests ok

---
+++ yatube/posts/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    def __str__(self):
+        # выводим текст поста
+        return self.text
+
+    group = models.ForeignKey(
+        'Group',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts'
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+
+class Group(models.Model):
+    title = models.CharField('Название', max_length=200)
+    description = models.TextField()
+    slug = models.SlugField(max_length=20, unique=True,)
+
+    def __str__(self) -> str:
+        return self.title
---
+++ yatube/posts/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from .models import Group, Post, User
+from .forms import PostForm
+
+
+LIMIT_POSTS_ON_PAGE: int = 10
+
+
+def authorized_only(func):
+    def check_user(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return func(request, *args, **kwargs)
+        return redirect('/auth/login/')
+    return check_user
+
+
+def index(request):
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    posts_list = Post.objects.filter(author=user)
+    posts_count = Post.objects.filter(author=user).count()
+    paginator = Paginator(posts_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': user,
+        'page_obj': page_obj,
+        'posts_count': posts_count,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    posts_count = Post.objects.filter(author=post.author).count()
+    context = {
+        'post': post,
+        'posts_count': posts_count,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@authorized_only
+def post_create(request, is_edit=False):
+    form = PostForm(request.POST or None)
+    context = {
+        'form': form,
+        'is_edit': is_edit
+    }
+    if not form.is_valid():
+        return render(request, 'posts/create_post.html', context)
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
+
+    return redirect('posts:profile', request.user)
+
+
+@authorized_only
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if request.method == "POST":
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:post_detail', post_id)
+
+    context = {
+        'form': form,
+        'is_edit': True,
+    }
+    if post.author == request.user:
+        return render(request, 'posts/create_post.html', context)
+    return redirect('posts:post_detail', post_id)
