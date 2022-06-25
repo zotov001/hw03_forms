842d41d6e4e8dc3b8690c0d712d13a49dde6bf5c
Марк Зотов <zotov001@yandex.ru>
2022-06-26 00:51:23 +0300

    edit4.2 tests ok

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -84,15 +84,14 @@ def post_edit(request, post_id):
     """Редактирование поста."""
     post = get_object_or_404(Post, pk=post_id)
     form = PostForm(request.POST or None, instance=post)
-    if not form.is_valid():
+    if post.author == request.user:
         context = {
             'form': form,
             'is_edit': True
         }
-        return render(request, 'posts/create_post.html', context)
-    if post.author == request.user and request.method == "POST":
-        post = form.save(commit=False)
-        post.author = request.user
-        post.save()
-        return redirect('posts:post_detail', post_id)
-    return redirect('posts:post_detail', post_id)
+        if request.method == 'POST' and form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:post_detail', post_id)
+    return render(request, "posts/create_post.html", context)
