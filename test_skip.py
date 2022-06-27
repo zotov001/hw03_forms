c01ad8b9b4fff7678bd044f57866085c2daaa051
Марк Зотов <zotov001@yandex.ru>
2022-06-27 21:13:04 +0300

    edit6.1

--- test_skip.py
+++ test_skip.py
@@ -9,15 +9,17 @@ class TestExample(unittest.TestCase):
     def test_show_msg(self):
         self.assertTrue(False, 'Значение должно быть истинным')
 
-    @unittest.skipIf(sys.version_info.major == 3 and sys.version_info.minor == 9,
-                     'Пропускаем, если питон 3.9')
+    @unittest.skipIf(
+        sys.version_info.major == 3 and sys.version_info.minor == 9, 'pyt3.9')
     def test_python3_9(self):
         # Тест будет запущен, только если версия питона отлична от 3.9.
-        # В условиях можно проверять версии библиотек, доступность внешних сервисов,
+        # В условиях можно проверять версии библиотек,
+        # доступность внешних сервисов,
         # время или дату - любые данные
         pass
 
-    @unittest.skipUnless(sys.platform.startswith('linux'), 'Тест только для Linux')
+    @unittest.skipUnless(sys.platform.startswith(
+        'linux'), 'Тест только для Linux')
     def test_linux_support(self):
         # Тест будет запущен только в Linux
         pass
